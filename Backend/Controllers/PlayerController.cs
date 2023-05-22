a8edf7c1337de30061850f6509a6e1bc819fbc8f
Roland Hajós <hajosr7@gmail.com>
2023-05-22 16:18:55 +0200

    add PlayerController

---
+++ Backend/Controllers/PlayerController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Model;
+using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("/players]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly IPlayerService _playerService;
+
+        public PlayerController(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        [HttpGet("getAllPlayers")]
+        public async Task<List<Player>> GetAllPlayers()
+        {
+            return await _playerService.GetAllPlayers();
+        }
+        
+        [HttpGet("getGoalkeepers")]
+        public async Task<List<Player>> GetGoalkeepers()
+        {
+            return await _playerService.GetGoalKeepers();
+        }
+        
+        [HttpGet("getDefenders")]
+        public async Task<List<Player>> GetDefenders()
+        {
+            return await _playerService.GetDefenders();
+        }
+        
+        [HttpGet("getMidfielders")]
+        public async Task<List<Player>> GetMidfielders()
+        {
+            return await _playerService.GetMidfielders();
+        }
+        
+        [HttpGet("getForwards")]
+        public async Task<List<Player>> GetForwards()
+        {
+            return await _playerService.GetForwards();
+        }
+
+        [HttpDelete("delete/{playerId}")]
+        public async Task<List<Player>> DeletePlayer(long playerId)
+        {
+            await _playerService.DeletePlayer(playerId);
+            return await _playerService.GetAllPlayers();
+        }
+    }
+}

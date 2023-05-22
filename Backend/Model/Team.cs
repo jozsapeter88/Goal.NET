7b5e293daae389382c8a4b272348f4f07d2fe19c
jozsapeter88 <jozsapeter88@gmail.com>
2023-05-22 17:01:20 +0200

    make Team properties nullable

--- Backend/Model/Team.cs
+++ Backend/Model/Team.cs
@@ -7,11 +7,11 @@ public class Team
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
     public string Name { get; set; }
-    public Coach Coach { get; set; }
+    public Coach? Coach { get; set; }
     // public Player GoalKeeper { get; set; }
-    public List<Player> AllPlayers { get; set; }
+    public List<Player>? AllPlayers { get; set; }
     // public List<Player> Midfielders { get; set; }
     // public List<Player> Forwards { get; set; }
-    public int Overall { get; set; }
-    public string Color { get; set; }
+    public int? Overall { get; set; }
+    public string? Color { get; set; }
 }

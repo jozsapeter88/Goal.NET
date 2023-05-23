0dadd730b925c25199e23bf0cb8f33b0028547b1
jozsapeter88 <jozsapeter88@gmail.com>
2023-05-23 13:45:31 +0200

    change Id type to long

--- Backend/Model/Team.cs
+++ Backend/Model/Team.cs
@@ -5,7 +5,7 @@ namespace Backend.Model;
 public class Team
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    public long Id { get; set; }
     public string Name { get; set; }
     public Coach Coach { get; set; }
     public List<Player> AllPlayers { get; set; }

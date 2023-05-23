737943552f758b9039db83a52879d44c694563b4
jozsapeter88 <jozsapeter88@gmail.com>
2023-05-23 13:58:59 +0200

    add nullable Team properties

--- Backend/Model/Team.cs
+++ Backend/Model/Team.cs
@@ -7,8 +7,8 @@ public class Team
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
     public string Name { get; set; }
-    public Coach Coach { get; set; }
-    public List<Player> AllPlayers { get; set; }
-    public int Overall { get; set; }
+    public Coach? Coach { get; set; }
+    public List<Player>? AllPlayers { get; set; }
+    public int? Overall { get; set; }
     public string Color { get; set; }
 }

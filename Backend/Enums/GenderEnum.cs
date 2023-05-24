c6ecd4ef8ff32f6c1d312b7ce0d2b32515f8c30b
jozsapeter88 <jozsapeter88@gmail.com>
2023-05-24 14:57:45 +0200

    update to PlayerCreator

--- Backend/Enums/GenderEnum.cs
+++ Backend/Enums/GenderEnum.cs
@@ -2,7 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace Backend.Enums;
 [JsonConverter(typeof(JsonStringEnumConverter))]
-public enum Gender
+public enum GenderEnum
 {
     Male,
     Female

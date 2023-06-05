f691da74c86fb0a8ae50436bc3833412f5d87911
jozsapeter88 <jozsapeter88@gmail.com>
2023-06-05 15:30:23 +0200

    add missing navbar

--- frontend/src/Components/TeamManager/TeamManager.jsx
+++ frontend/src/Components/TeamManager/TeamManager.jsx
@@ -1,9 +1,11 @@
+import Menu from "../Menu/Menu";
 import CreateSection from "./CreateSection";
 import ManageSection from "./ManageSection";
 
 const HomePage = () => {
   return (
     <div>
+      <Menu />
       <CreateSection />
       <ManageSection />
     </div>

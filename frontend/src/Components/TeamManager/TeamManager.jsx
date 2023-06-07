bfe280de869bd94b46f50c79e8936e8b6679cc74
jozsapeter88 <jozsapeter88@gmail.com>
2023-06-07 14:49:20 +0200

    add TeamManager modification

--- frontend/src/Components/TeamManager/TeamManager.jsx
+++ frontend/src/Components/TeamManager/TeamManager.jsx
@@ -1,21 +1,38 @@
-import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import Menu from "../Menu/Menu";
 import CreateSection from "./CreateSection";
 import ManageSection from "./ManageSection";
 
-const TeamManager = () => {
+const TeamManager = (teams) => {
+
+  // If no teams are found
+  if (teams.length === 0) {
+    return (
+      <Card bg="secondary" style={{ color: "white" }}>
+        <Card.Body>
+          <Card.Title>No teams found</Card.Title>
+          <Card.Text>
+            Go to the Team Manager to create a team
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
+
+  // If teams are found
   return (
     <>
       <Menu />
       <div className="MyTeams">
-      <h1 style={{ color: "white", fontSize: "2.5rem", fontWeight: "bold", textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}>Team Manager</h1>
+        <h1 style={{ color: "white", fontSize: "2.5rem", fontWeight: "bold", textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}>Team Manager</h1>
       </div>
       <Container>
         <Row>
           <Col md={1}>
             <CreateSection />
           </Col>
-          <Col md={10} style={{padding: 30, marginLeft: 0}}>
+          <Col md={10} style={{ padding: 30, marginLeft: 0 }}>
             <ManageSection />
           </Col>
         </Row>

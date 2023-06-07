7bd3a5f3f282c0b44c0879c32c300c2f4cc37ea3
Roland Hajós <hajosr7@gmail.com>
2023-06-07 13:58:40 +0200

    add exception handling to TeamController.cs

--- Backend/Services/ITeamService.cs
+++ Backend/Services/ITeamService.cs
@@ -5,16 +5,17 @@ namespace Backend.Services;
 
 public interface ITeamService
 {
-    Task<List<Team>> GetAllTeams();
-    Task<Team> GetTeam(long teamId);
-    Task<List<Team>> GetTeamsOfUser(long userId);
-    Task<List<Player>> GetPlayersOfTeam(long userId, long teamId);
+    Task<List<Team>?> GetAllTeams();
+    Task<Team?> GetTeam(long teamId);
+    Task<List<Team>?> GetTeamsOfUser(long userId);
+    Task<List<Player>?> GetPlayersOfTeam(long userId, long teamId);
 
-    Task<Team> CreateTeam(TeamCreateDto team);
-    Task<Team> UpdateTeam(long teamId, Team team);
+    Task<List<Team>> CreateTeam(TeamCreateDto team);
+    Task<Team?> UpdateTeam(long teamId, Team team);
     Task<List<Team>> DeleteTeam(long teamId);
+    Task<List<Team>> UserDeleteTeam(long userId, long teamId);
 
-    Task<Team> AddPlayerToTeam(long userId, long teamId, long playerId);
+    Task<Team?> AddPlayerToTeam(long userId, long teamId, long playerId);
 
-    Task<List<Team>> AddTeamToUser(long userId, TeamCreateDto team);
+    Task<List<Team>?> AddTeamToUser(long userId, TeamCreateDto team);
 }

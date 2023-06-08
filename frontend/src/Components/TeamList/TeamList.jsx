c1cc02560b8c1bfe6b1eeb455c4bf0aa925d0b1e
jozsapeter88 <jozsapeter88@gmail.com>
2023-06-08 10:57:12 +0200

    add TeamList component

---
+++ frontend/src/Components/TeamList/TeamList.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import "./TeamList.css";
+
+const TeamList = () => {
+  return (
+    <div className="gallery">
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+      <div className="teamCard">
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          <p>Position </p>
+          <p>Nationality </p>
+          <p>Overall</p>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TeamList;

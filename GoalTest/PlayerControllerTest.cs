45809ca1a6ad551ee7fab4a2bbde8a7bc46afd92
jozsapeter88 <jozsapeter88@gmail.com>
2023-06-20 11:25:29 +0200

    add PlayerControllerTest class

--- GoalTest/PlayerControllerTest.cs
+++ GoalTest/PlayerControllerTest.cs
@@ -1,6 +1,57 @@
-﻿namespace GoalTest;
+﻿using AutoMapper;
+using Backend.Controllers;
+using Backend.DTOs;
+using Backend.Enums;
+using Backend.Model;
+using Backend.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 
-public class PlayerControllerTest
+namespace GoalTest
 {
-    
+    [TestFixture]
+    public class PlayerControllerTest
+    {
+        private PlayerController _playerController;
+        private IPlayerService _playerService;
+        private GoalContext _dbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<GoalContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _dbContext = new GoalContext(options);
+
+            _dbContext.Players.AddRange(
+                new Player { Name = "TestPlayer1", Position = PositionEnum.Forward },
+                new Player { Name = "TestPlayer2", Position = PositionEnum.Defender }
+            );
+            _dbContext.SaveChanges();
+
+            _playerService = new PlayerService(_dbContext, Mock.Of<IMapper>());
+            _playerController = new PlayerController(_playerService);
+        }
+
+        [Test]
+        public async Task GetAllPlayer_ReturnListOfPlayerDto()
+        {
+            // Act
+            var result = await _playerController.GetAllPlayers();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<List<PlayerDto>>(result);
+            Assert.That(result.Count, Is.EqualTo(2));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _dbContext.Database.EnsureDeleted();
+            _dbContext.Dispose();
+        }
+    }
 }

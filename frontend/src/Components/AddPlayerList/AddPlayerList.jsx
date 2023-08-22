966701b2207bdf6b95070ba8f891c77bd5eccefa
Roland Hajós <hajosr7@gmail.com>
2023-08-22 14:19:32 +0200

    add addplayerlist

---
+++ frontend/src/Components/AddPlayerList/AddPlayerList.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./AddPlayerList.css";
+import Loading from "../Loading";
+
+
+const AddPlayerList = ({players, loadingPlayers}) => {
+  if (loadingPlayers) {
+    return <Loading />;
+  }
+  return (
+    <>
+    
+    <div className="gallery">
+    {players?players.map((p) => (
+      <div className="teamCard" key={p.id}>
+        <h3 className="teamCard-title">Card</h3>
+        <p className="teamCard-description">
+          
+          <p>{p.name}</p>
+          
+          <p>{p.nationality}</p>
+          
+          <p>{p.score}</p>
+        </p>
+      </div>
+       )): <div><p>No Players</p></div>}
+      </div>
+      
+   </>)
+};
+
+export default AddPlayerList;

39000c13d58202dd6e5366a67f35bb7709688244
TaurusBoss <szova.kristof@gmail.com>
2023-09-06 12:55:08 +0200

    maximize compatibility of auth.js

--- frontend/src/auth/auth.js
+++ frontend/src/auth/auth.js
@@ -5,15 +5,19 @@ import useCookies from "react-cookie/cjs/useCookies";
 const auth = (minimumLevel) => {
     const [cookies, setCookie, removeCookie] = useCookies();
     const levels = cookies["userlevels"];
-    console.log(levels)
+    const level = capitalize(cookies["userlevel"].toLowerCase());
     const isLevelANumber = Number.isInteger(minimumLevel);
     if (!isLevelANumber && levels.includes(minimumLevel)) minimumLevel = levels.indexOf(minimumLevel)
     else if (!isLevelANumber){
         console.error("Error: provided minimum level is not a valid parameter");
         return false;
     }
-    if (!levels.includes(cookies["userlevel"])) return false;
-    else return levels.indexOf(cookies["userlevel"]) >= minimumLevel
+    if (!levels.includes(level)) return false;
+    else return levels.indexOf(level) >= minimumLevel
+}
+
+function capitalize(level) {
+    return level[0].toUpperCase() + level.substring(1)
 }
 
 export default auth;
